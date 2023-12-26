1f4a9747f0d338939a822362b5ad457c7b54252d
RSM <rsmRSM@Michels-iMac.local>
2023-12-26 15:15:23 -0500

    update

--- epd.cpp
+++ epd.cpp
@@ -3,6 +3,8 @@
 #include "font5x5.h"
 #include "font8x8.h"
 
+#include "src/gj/src/gj/base.h"
+#include "src/gj/src/gj/eventmanager.h"
 
 /*
 
@@ -23,9 +25,7 @@ static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI i
 static volatile bool spi_xfer_done = true;  /**< Flag used to indicate that SPI instance completed the transfer. */
 
 #define TEST_STRING "Nordic"
-static uint8_t       m_tx_buf[] = TEST_STRING;           /**< TX buffer. */
 static uint8_t       m_rx_buf[sizeof(TEST_STRING) + 1];    /**< RX buffer. */
-static const uint8_t m_length = sizeof(m_tx_buf);        /**< Transfer length. */
 
 /**
  * @brief SPI user event handler.
@@ -69,8 +69,6 @@ extern "C"
       
 
   }
-
-  void epd_update_partial();
 }
 
 
@@ -106,7 +104,6 @@ void write_data(uint8_t c)
 {
     spi_write(&c, sizeof(c));
 }
-
 
 void write_data(Epaper &epd, uint8_t c)
 {
@@ -145,7 +142,9 @@ void write_command_and_args(Epaper &epd, uint8_t c, uint8_t a, uint8_t a2)
 
 uint8_t epd_is_busy(Epaper &epd)
 {
-  return ReadPin(epd.m_pins.m_busy) != 0;
+  const bool busy = ReadPin(epd.m_pins.m_busy) != 0;
+
+  return busy;
 }
 
 uint8_t epd_wait_busy(Epaper &epd)
@@ -166,7 +165,7 @@ uint8_t epd_wait_busy(Epaper &epd)
       printf("timeout\n\r");
       return 1;
     }
-    Delay(1);
+    Delay(100);
   }
 
   if (timeout >= printTimeout)
@@ -177,6 +176,41 @@ uint8_t epd_wait_busy(Epaper &epd)
   return 0;
 }
 
+void epd_wait_busy_async(Epaper *epd)
+{
+  if (epd_is_busy(*epd))
+  {
+    if (epd->m_waitCount >= 20)
+    {
+      (*epd->m_cb)(epd, epd->m_cookie, false);
+      return;
+    }
+
+    auto event = [=]()
+    {
+      epd_wait_busy_async(epd);
+    };
+
+    const uint32_t delay = epd->m_cbDelay;
+    epd->m_cbDelay = 100 * 1000;
+
+    GJEventManager->DelayAdd(event, delay);
+  }
+  else
+  {
+    (*epd->m_cb)(epd, epd->m_cookie, true);
+  }
+}
+
+void epd_wait_busy_async(Epaper &epd, EpdCallback cb, uint32_t cookie)
+{
+  epd.m_cb = cb;
+  epd.m_cookie = cookie;
+  epd.m_waitCount = 0;
+  epd.m_cbDelay = 2000 * 1000;
+
+  epd_wait_busy_async(&epd);
+}
 
 void SetCursor(Epaper &epd, uint16_t x, uint16_t y)
 {
@@ -204,7 +238,7 @@ void SetViewport(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h)
 
 }
 
-void FillBox8(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t innerCol, uint8_t outerCol)
+static void FillBox8_Internal(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t innerCol, uint8_t outerCol)
 {
   //SetViewport(epd, x, y, w, h);
   
@@ -262,9 +296,24 @@ void FillBox8(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8
   //    write_data(epd, innerCol);
   //}
 
+}
+
+void FillBox8(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t innerCol, uint8_t outerCol)
+{
+  FillBox8_Internal(epd, x, y, w, h, innerCol, outerCol);
+
+  bool b = epd_is_busy(epd);
 
   epd_wait_busy(epd);
 }
+
+void FillBox8(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t innerCol, uint8_t outerCol, EpdCallback cb, uint32_t cookie)
+{
+  FillBox8_Internal(epd, x, y, w, h, innerCol, outerCol);
+
+  epd_wait_busy_async(epd, cb, cookie);
+}
+
 
 void FillBox(Epaper &epd, uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t colBit)
 {
@@ -279,18 +328,27 @@ void ClearEpd(Epaper &epd, uint8_t colBit)
   FillBox(epd, 0, 0, 122, 250, colBit);
 }
 
-void RefreshEdp(Epaper &epd)
+bool RefreshEdp(Epaper &epd)
 {
   static uint8_t data22[] = {0xf7};
   write_command_and_data(epd, 0x22, data22, sizeof(data22)); //  Display Update Control
 
   write_command(epd, 0x20); // Activate Display Update Sequence
 
-  epd_wait_busy(epd);
+  return epd_wait_busy(epd) == 0;
 }
 
+void RefreshEdp(Epaper &epd, EpdCallback cb, uint32_t cookie)
+{
+  static uint8_t data22[] = {0xf7};
+  write_command_and_data(epd, 0x22, data22, sizeof(data22)); //  Display Update Control
 
-void RefreshPartialEdp(Epaper &epd)
+  write_command(epd, 0x20); // Activate Display Update Sequence
+
+  epd_wait_busy_async(epd, cb, cookie);
+}
+
+bool RefreshPartialEdp(Epaper &epd)
 {
   
   //SetCursor(epd, 0, 0);
@@ -300,7 +358,7 @@ void RefreshPartialEdp(Epaper &epd)
 
   write_command(epd, 0x20); // Activate Display Update Sequence
 
-  epd_wait_busy(epd);
+  return epd_wait_busy(epd) == 0;
 }
 
 struct Font
@@ -441,8 +499,19 @@ void InitEpdSpi(int clk, int data)
     spi_config.mosi_pin = data;
     spi_config.sck_pin  = clk;
     spi_config.frequency  = NRF_DRV_SPI_FREQ_8M;
+    spi_config.frequency  = NRF_DRV_SPI_FREQ_125K;
     //APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, spi_event_handler));
     APP_ERROR_CHECK(nrf_drv_spi_init(&spi, &spi_config, nullptr));    
+  }
+
+}
+
+void TermEpdSpi()
+{
+  if (epdInit == true)
+  {
+    epdInit = false;
+    nrf_drv_spi_uninit(&spi);    
   }
 
 }
@@ -469,7 +538,7 @@ static const unsigned char ut_partial[] =
   0x22, 0x22, 0x22, 0x22, 0x22, 0x22, 0x0, 0x0, 0x0,
 };
 
-void InitPartialEpd(Epaper &epd)
+bool InitPartialEpd(Epaper &epd)
 {
   InitEpd(epd);
 
@@ -481,11 +550,10 @@ void InitPartialEpd(Epaper &epd)
     write_data(ut_partial[j]);
   }
   write_data_end(epd);
-  epd_wait_busy(epd);
-
+  return epd_wait_busy(epd) == 0;
 }
 
-void ResetEpd(Epaper &epd)
+void ResetEpd(Epaper &epd, bool wait)
 {
   //HW reset
   WritePin(epd.m_pins.m_reset, 0);
@@ -493,16 +561,20 @@ void ResetEpd(Epaper &epd)
   WritePin(epd.m_pins.m_reset, 1);
   Delay(100);
 
-  epd_wait_busy(epd);
+  if (wait)
+    epd_wait_busy(epd);
 }
 
-void InitEpd(Epaper &epd)
+bool InitEpd(Epaper &epd)
 {  
-  epd_wait_busy(epd);
+  if (epd_wait_busy(epd) != 0)  
+    return false;
 
   write_command(epd, 0x12);
   Delay(10);
-  epd_wait_busy(epd);
+
+  if (epd_wait_busy(epd) != 0)  
+    return false;
 
   static uint8_t data01[] = {0x27, 0x01, 0x00};
   write_command_and_data(epd, 0x01, data01, sizeof(data01)); // Driver output control
@@ -520,8 +592,11 @@ void InitEpd(Epaper &epd)
 
   SetCursor(epd, 0, 0);
   
-  epd_wait_busy(epd);
+  if (epd_wait_busy(epd) != 0)  
+    return false;
 
   epd.m_rt = 0;
+
+  return true;
 }
 
